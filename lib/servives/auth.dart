a6c2e94f55e4808dc758db85877701fde4e897f7
iqrafatimame <candy.iqo@gmail.com>
2020-10-18 12:05:24 -0700

    Setting up enviroment

---
+++ lib/servives/auth.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  // todo:
+  // 1: Sign in anonymously
+
+  // 2: Sign in with email and Password
+
+  //3: Register with Email and Password
+
+  //4: Signout
+
+}
